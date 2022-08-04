159f8fc8c23f8a8fd85c69992dfa61902c276341
AnshGoyal7310 <110545267+AnshGoyal7310@users.noreply.github.com>
2022-08-03 20:06:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function val(result) {
+  form.disp.value = form.disp.value + result;
+}
+function calculate() {
+  if (form.disp.value == "") {
+    alert("Please Enter Numbers");
+  } else {
+    form.disp.value = eval(form.disp.value);
+  }
+}
+var btn = form.veql;
+btn.addEventListener("dblclick", function () {
+  form.disp.value = "";
+});
